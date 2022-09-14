eb9a7418077103c5d8f30a4f14622702e5aca3b1
Anurag <kr.anurag24@gmail.com>
2022-09-14 18:06:18 +0530

    yes

---
+++ templates/node-express/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 500;
+
+app.listen(PORT, () => {
+  console.log("Listening on port 5000");
+});
