e88026d7ef4e5cbaa645d00bd1797a64b610e410
Anurag <kr.anurag24@gmail.com>
2022-11-04 19:41:36 +0530

    updates

--- templates/next/src/pages/_app.tsx
+++ templates/next/src/pages/_app.tsx
@@ -1,12 +1,17 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@/theme";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
